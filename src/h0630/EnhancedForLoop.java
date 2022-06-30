0930c485d8863fc65f727c7525b35ae3e36d37d6
0412corn@naver.com <0412corn@naver.com>
2022-06-30 22:04:19 +0900

    향상된 for문

---
+++ src/h0630/EnhancedForLoop.java
@@ -0,0 +1,14 @@
+package h0630;
+
+public class EnhancedForLoop {
+
+	public static void main(String[] args) {
+		String [] strArray = {"사과","레몬","복숭아"};
+		
+		for(String fruits : strArray) {
+			System.out.println(fruits);
+		}
+
+	}
+
+}
