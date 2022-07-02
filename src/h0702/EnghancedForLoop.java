69276692068443e78331664344ee835980e50a67
0412corn@naver.com <0412corn@naver.com>
2022-07-02 15:05:42 +0900

    향상된 for문 사용하기

---
+++ src/h0702/EnghancedForLoop.java
@@ -0,0 +1,14 @@
+package h0702;
+
+public class EnghancedForLoop {
+
+	public static void main(String[] args) {
+
+			String [] strArray = {"복숭아","레몬","사과"}; //초기값 지정 
+			
+			for(String fruits : strArray) {
+				System.out.println(fruits);
+			}
+	}
+
+}
