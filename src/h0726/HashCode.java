0d0de8d46bc7d9c3c442fc26319dfb6a59fc43d1
0412corn@naver.com <0412corn@naver.com>
2022-07-26 21:28:35 +0900

    String,Integer - hashCode() 반환

---
+++ src/h0726/HashCode.java
@@ -0,0 +1,19 @@
+package h0726;
+
+public class HashCode {
+	public static void main(String[] args) {
+		String str1 = new String("abc");
+		String str2 = new String("abc");
+		
+		System.out.println(str1.hashCode()); //abc 문자열의 해시 코드 값 출력
+		System.out.println(str2.hashCode());
+		
+		Integer i1 = new Integer(100);
+		Integer i2 = new Integer(200);
+		
+		System.out.println(i1.hashCode()); //Integer(100)의 해시 코드 값 출력
+		System.out.println(i2.hashCode());
+		
+	}
+
+}
