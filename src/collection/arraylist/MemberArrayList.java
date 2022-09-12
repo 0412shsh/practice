170debd17ad4df5f7a8de2ef7081f78c6cad2f0d
0412corn@naver.com <0412corn@naver.com>
2022-09-12 11:23:53 +0900

    ArrayList

--- src/collection/arraylist/MemberArrayList.java
+++ src/collection/arraylist/MemberArrayList.java
@@ -4,42 +4,35 @@ import java.util.ArrayList;
 
 import collection.Member;
 
-public class MemberArrayList {
-	private ArrayList<Member> arrayList; //  ArrayList 선언 
-
-
-public MemberArrayList() {
-	arrayList = new ArrayList<Member>(); //Member형으로 선언한 ArrayList 생성 
-	}
-
-public void addMember(Member member) {
-	arrayList.add(member); //ArrayList에 회원을 추가하는 메서드
-}
-
-public void insertMember(Member member, int index) {
-	arrayList.add(index, member);  //원하는 위치에 회원 삽입
+	public class MemberArrayList {
+	private ArrayList<Member> arrayList; //ArrayList 선언
 	
-}
-
-
-public boolean removeMember(int memberId) {
-	for(int i = 0; i<arrayList.size(); i++) {
-		Member member = arrayList.get(i); 
-		int tempId = member.getMemberId();
-		if(tempId == memberId){
-			arrayList.remove(i); 
-			return true; 
+	public MemberArrayList() {
+		arrayList = new ArrayList<Member>(); //Member형으로 선언한 ArrayList 생성
+	}
+	
+	public void addMember(Member member) {
+		arrayList.add(member);
+	}  //ArrayList에 회원을 추가하는 메서드
+	
+	public boolean removeMember(int memberId) {
+		for(int i = 0; i<arrayList.size(); i++) { //get()메서드로 회원을 순차적으로 가져옴
+			Member member = arrayList.get(i);	
+			int tempId = member.getMemberId(); 	
+			if(tempId == memberId) { //회원 아이디가 매개변수와 일치하면
+				arrayList.remove(i); //해당회원을 삭제
+				return true;
+			}
 		}
+		System.out.println(memberId+"가 존재하지 않습니다");
+		return false; //반복문이 끝날때까지 해당 아이디를 찾지 못한 경우
 	}
-	System.out.println(memberId+"가 존재하지 않습니다.");
-	return false; 
 	
-}
-
 	public void showAllMember() {
 		for(Member member : arrayList) {
 			System.out.println(member);
 		}
 		System.out.println();
-	}
+	} //전체 회원을 출력하는 메서드 
+	
 }
--- src/collection/arraylist/MemberArrayListTest.java
+++ src/collection/arraylist/MemberArrayListTest.java
@@ -1,34 +1,30 @@
 package collection.arraylist;
 
 import collection.Member;
+
 public class MemberArrayListTest {
 
 	public static void main(String[] args) {
-		MemberArrayList memberArrayList = new MemberArrayList(); //MemberArrayList 객체생성 
+		MemberArrayList memberArrayList = new MemberArrayList();
 		
-		Member memberLee = new Member(1001, "이레몬");
-		Member memberSon = new Member(1002, "손사과");
-		Member memberPark = new Member(1003, "박복숭");
-		Member memberHong = new Member(1004, "홍 시"); //Member형의 새로운 회원 인스턴스 생성
-		Member memberGo = new Member(1005, "고감자"); //감자학생 추가
+		Member memberLee = new Member(1001, "이지원");
+		Member memberSon = new Member(1002, "손민국");
+		Member memberPark = new Member(1003,"박서현");
+		Member memberHong = new Member(1004, "홍길동"); //새로운 회원 인스턴스 생성
 		
-		memberArrayList.addMember(memberLee); //0
-		memberArrayList.addMember(memberSon); //1
-		memberArrayList.addMember(memberPark); //2
-		memberArrayList.addMember(memberHong); //ArrayList에 회원 추가 ,3 
-		
-		memberArrayList.insertMember(memberGo, 1); //고감자 새치기 
+		memberArrayList.addMember(memberLee);
+		memberArrayList.addMember(memberSon);
+		memberArrayList.addMember(memberPark);
+		memberArrayList.addMember(memberHong); //ArrayList에 회원 추가
 		
 		memberArrayList.showAllMember(); //전체 회원 출력
 		
 		memberArrayList.removeMember(memberHong.getMemberId());
 		memberArrayList.showAllMember();
 		
-//		memberArrayList.removeMember(memberHong.getMemberId());
-//		memberArrayList.showAllMember();
 		
 		
-	
+		
 	}
 
 }
